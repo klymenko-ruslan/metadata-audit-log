d6128de8ebf3145bc1a43766507da88a2e45539a
Ruslan Klymenko <ruslan.klymenko@zorallabs.com>
2020-07-02 14:44:47 +0300

    [Bugfix] - Missed partId fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var logService = require('../services/log-service');
 var router = express.Router();
 
 router.post('/log', function(req, res, next) {
-    logService.insertRecord([req.body.partId], req.body.oldHeader, req.body.newHeader);
+    partIds = (typeof req.body.partId === 'undefined') ? [] : [req.body.partId];
+    logService.insertRecord(partIds, req.body.oldHeader, req.body.newHeader);
     res.end("");
 });
 router.post('/log-group', function(req, res, next) {

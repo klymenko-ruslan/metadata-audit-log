47001da6cf3ae52759b69e5721b1e2dee69e88c6
rklymenko <klymenko.ruslan.dev@gmail.com>
2020-07-15 11:22:01 +0300

    [1226] - Refactorings

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var logService = require('../services/log-service');
 var router = express.Router();
 
-router.post('/log/create', function(req, res, next) {
+router.post('/log/interchange/create', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     res.end(logService.create(req.body));
 
 });
-router.post('/log/add', function(req, res, next) {
+router.post('/log/interchange/add', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.add(req.body).then(response => {
         res.end(response);
     });
 
 });
-router.post('/log/leave', function(req, res, next) {
+router.post('/log/interchange/leave', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.leave(req.body).then(response => {
         res.end(response);
@@ -22,7 +22,7 @@ router.post('/log/leave', function(req, res, next) {
 
 
 });
-router.post('/log/merge', function(req, res, next) {
+router.post('/log/interchange/merge', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.merge(req.body).then(response => {
         res.end(response);
--- services/log-service.js
+++ services/log-service.js
@@ -2,7 +2,7 @@ const { uuid } = require('uuidv4');
 const config = require('config');
 const mysql = require('mysql2');
 
-var pool = mysql.createPool({
+let pool = mysql.createPool({
   host: config.get("datasource.host"),
   user: config.get("datasource.user"),
   password: config.get("datasource.password"),
@@ -10,55 +10,61 @@ var pool = mysql.createPool({
   connectionLimit: 10
 });
 
-function insertRecord(partIds, oldHeader, newHeader) {
-    var generatedUuid = uuid();
+let generateResponse = (description, transactionId) => JSON.stringify({ 'description' : description, 'transactionId': transactionId});
+
+let insertRecords = async (partIds, oldHeader, newHeader) => {
+    let generatedUuid = uuid();
     partIds = (typeof partIds === 'undefined') ? [] : partIds;
     oldHeader = (typeof oldHeader === 'undefined') ? null : oldHeader;
     newHeader = (typeof newHeader === 'undefined') ? null : newHeader;
-    pool.getConnection(function(err, con) {
-      if (err) throw err;
-      var sql = "INSERT INTO interchange_audit_log(transaction_id, old_header, new_header) values('" + generatedUuid + "', " + oldHeader + ", " + newHeader + ")";
-        con.query(sql, function (err, result) {
-            console.log(result);
-          if (err) throw err;
-          partIds.forEach(function(partId) {
-              var interchangeAuditLogTransactionsQuery = "INSERT INTO interchange_audit_log_transactions(transaction_id, part_id) values('" + generatedUuid + "', " + partId + ")";
-              con.query(interchangeAuditLogTransactionsQuery, function (err, result) {
-                if (err) throw err;
-              });
-          });
-
-        });
-        con.release();
-    });
+    await insertLog(partIds, oldHeader, newHeader, generatedUuid);
+    await insertTransaction(generatedUuid, partIds);
     return generatedUuid;
 }
 
-function create(body) {
-    var transactionId = insertRecord([body.partId], body.oldHeader, body.newHeader);
-    return JSON.stringify({ 'description' : 'Created interchange: [' + body.partId  + '].', 'transactionId': transactionId});
-}
-async function leave(body) {
-    var transactionId = insertRecord([body.partId], body.oldHeader, body.newHeader);
-    var con = pool.promise();
-    const [rows,fields] = await con.query('SELECT manfr_part_num FROM part WHERE id = ' + body.partId);
-    return JSON.stringify({ 'description' : 'The part [[' + body.partId + '] - ' + rows[0].manfr_part_num + '] migrated from interchange group [' + body.oldHeader + '] to [' + body.newHeader + '].', 'transactionId': transactionId});
+let insertLog = async (partIds, oldHeader, newHeader, generatedUuid) => {
+    let con = pool.promise();
+    const [rows,] = await con.query(`INSERT INTO interchange_audit_log(transaction_id, old_header, new_header) values('${generatedUuid}', ${oldHeader}, ${newHeader})`);
+    pool.releaseConnection(con);
+    return rows;
 }
 
-async function add(body) {
-    var transactionId = insertRecord([body.partId], body.oldHeader, body.newHeader);
-    var con = pool.promise();
-    const [fromRows, fromFields] = await con.query('SELECT manfr_part_num FROM part WHERE id = ' + body.partId);
-    const [toRows, toFields] = await con.query('SELECT manfr_part_num FROM part WHERE id = ' + body.toPartId);
-    return JSON.stringify({ 'description' : 'Part [' + body.partId + '] - ' + fromRows[0].manfr_part_num + ' added to the part [' + body.toPartId + '] - ' + toRows[0].manfr_part_num + ' as interchange.', 'transactionId': transactionId});
+let insertTransaction = async (transactionId, partIds) => {
+    partIds.forEach(function(partId) {
+        let con = pool.promise();
+        con.query(`INSERT INTO interchange_audit_log_transactions(transaction_id, part_id) values('${transactionId}', ${partId})`);
+        pool.releaseConnection(con);
+    });
 }
 
-async function merge(body) {
-    var transactionId = insertRecord(body.partIds, body.oldHeader, body.newHeader);
-    var con = pool.promise();
-    const [fromRows, fromFields] = await con.query('SELECT manfr_part_num FROM part WHERE id = ' + body.partIds[1]);
-    const [toRows, toFields] = await con.query('SELECT manfr_part_num FROM part WHERE id = ' + body.partIds[0]);
-    return JSON.stringify({ 'description' : 'Part [' + body.partIds[1] + '] - ' + fromRows[0].manfr_part_num + ' and all its interchanges[' + body.partIds.slice(2) + '] added to the part [' + body.partIds[0] + '] - ' + toRows[0].manfr_part_num + ' as interchanges.', 'transactionId': transactionId});
+let getPartNumberById = async (partId) => {
+    let con = pool.promise();
+    const [rows,] = await con.query('SELECT manfr_part_num FROM part WHERE id = 1')// + partId);
+    pool.releaseConnection(con);
+    return rows[0].manfr_part_num;
+}
+
+let create = async (body) => {
+    let transactionId = await insertRecords([body.partId], body.oldHeader, body.newHeader);
+    let description = `Created interchange: [${body.partId}].`;
+    return generateResponse(description, transactionId);
+}
+let leave = async (body) => {
+    let transactionId = await insertRecords([body.partId], body.oldHeader, body.newHeader);
+    let description = `The part [[${body.partId}] - ${await getPartNumberById(body.partId)}] migrated from interchange group [${body.oldHeader}] to [${body.newHeader}].`;
+    return generateResponse(description, transactionId);
+}
+
+let add = async (body) => {
+    let transactionId = await insertRecords([body.partId], body.oldHeader, body.newHeader);
+    let description = 'Part [' + body.partId + '] - ' + await getPartNumberById(body.partId) + ' added to the part [' + body.toPartId + '] - ' + await getPartNumberById(body.toPartId)+ ' as interchange.';
+    return generateResponse(description, transactionId);
+}
+
+let merge = async (body) => {
+    let transactionId = await insertRecords(body.partIds, body.oldHeader, body.newHeader);
+    let description = 'Part [' + body.partIds[1] + '] - ' + await getPartNumberById(body.partIds[1]) + ' and all its interchanges[' + body.partIds.slice(2) + '] added to the part [' + body.partIds[0] + '] - ' + await getPartNumberById(body.partIds[0]) + ' as interchanges.';
+    return generateResponse(description, transactionId);
 }
 
 module.exports.create = create;

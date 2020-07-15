9b4420cb436fa575dfb6d0943c8b94e151e53aca
kshakirov <kirill.shakirov4@gmail.com>
2020-07-15 14:15:54 +0300

    Some polishing, needed to be checked

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var logService = require('../services/log-service');
 var router = express.Router();
 
-router.post('/log/interchange/create', function(req, res, next) {
+router.post('/log/interchange/create', function(req, res, ) {
     res.setHeader('Content-Type', 'application/json');
     res.end(logService.create(req.body));
 
 });
-router.post('/log/interchange/add', function(req, res, next) {
+router.post('/log/interchange/add', function(req, res,) {
     res.setHeader('Content-Type', 'application/json');
     logService.add(req.body).then(response => {
         res.end(response);
     });
 
 });
-router.post('/log/interchange/leave', function(req, res, next) {
+router.post('/log/interchange/leave', function(req, res, ) {
     res.setHeader('Content-Type', 'application/json');
     logService.leave(req.body).then(response => {
         res.end(response);
@@ -22,7 +22,7 @@ router.post('/log/interchange/leave', function(req, res, next) {
 
 
 });
-router.post('/log/interchange/merge', function(req, res, next) {
+router.post('/log/interchange/merge', function(req, res, ) {
     res.setHeader('Content-Type', 'application/json');
     logService.merge(req.body).then(response => {
         res.end(response);

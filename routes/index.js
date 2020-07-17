864f49ffdc79f44f08564088efdbf1d6c9f2cd88
rklymenko <klymenko.ruslan.dev@gmail.com>
2020-07-17 14:58:36 +0300

    [1226] - Logging

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,22 @@ var router = express.Router();
 
 router.post('/log/interchange/create', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
-    res.end(logService.create(req.body));
-
+    logService.create(req.body).then(response => {
+        res.end(response);
+    }).catch(err => sendErrorResponse(res, err));
 });
 router.post('/log/interchange/add', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.add(req.body).then(response => {
         res.end(response);
-    });
+    }).catch(err => sendErrorResponse(res, err));
 
 });
 router.post('/log/interchange/leave', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.leave(req.body).then(response => {
         res.end(response);
-    });
+    }).catch(err => sendErrorResponse(res, err));
 
 
 });
@@ -26,7 +27,14 @@ router.post('/log/interchange/merge', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     logService.merge(req.body).then(response => {
         res.end(response);
-    });
+    }).catch(err => sendErrorResponse(res, err));
 });
+
+function sendErrorResponse(res, err) {
+    console.error(`Error: ${err}`);
+    return res.status(400).send({
+        msg: err
+    })
+}
 
 module.exports = router;

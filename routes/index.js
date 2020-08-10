8d6c57283ad5518fe38005e16169050085343613
rklymenko <klymenko.ruslan.dev@gmail.com>
2020-08-10 12:01:51 +0300

    [1226] - Logging

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,47 @@ var logService = require('../services/log-service');
 var router = express.Router();
 
 router.post('/log/interchange/create', function(req, res, ) {
-    res.setHeader('Content-Type', 'application/json');
-    logService.create(req.body).then(response => {
-        res.end(response);
-    }).catch(err => sendErrorResponse(res, err));
-});
-router.post('/log/interchange/add', function(req, res,) {
-    res.setHeader('Content-Type', 'application/json');
-    logService.add(req.body).then(response => {
-        res.end(response);
-    }).catch(err => sendErrorResponse(res, err));
+    try {
+        res.setHeader('Content-Type', 'application/json');
+        logService.create(req.body).then(response => {
+            res.end(response);
+        }).catch(err => sendErrorResponse(res, err));
+    } catch(e) {
+        console.log(e);
+    }
 
 });
+router.post('/log/interchange/add', function(req, res,) {
+    try {
+        res.setHeader('Content-Type', 'application/json');
+        logService.add(req.body).then(response => {
+            res.end(response);
+        }).catch(err => sendErrorResponse(res, err));
+    } catch(e) {
+        console.log(e);
+    }
+});
 router.post('/log/interchange/leave', function(req, res, ) {
-    res.setHeader('Content-Type', 'application/json');
-    logService.leave(req.body).then(response => {
-        res.end(response);
-    }).catch(err => sendErrorResponse(res, err));
+    try {
+        res.setHeader('Content-Type', 'application/json');
+        logService.leave(req.body).then(response => {
+            res.end(response);
+        }).catch(err => sendErrorResponse(res, err));
+    } catch(e) {
+        console.log(e);
+    }
 
 
 });
 router.post('/log/interchange/merge', function(req, res, ) {
-    res.setHeader('Content-Type', 'application/json');
-    logService.merge(req.body).then(response => {
-        res.end(response);
-    }).catch(err => sendErrorResponse(res, err));
+    try {
+        res.setHeader('Content-Type', 'application/json');
+        logService.merge(req.body).then(response => {
+            res.end(response);
+        }).catch(err => sendErrorResponse(res, err));
+    } catch(e) {
+        console.log(e);
+    }
 });
 
 function sendErrorResponse(res, err) {
